9d472b04504df9e6eea2560d57e696b07def616b
Pranav Kesavarapu <pranavkesavarapu@gmail.com>
2021-04-27 00:23:48 +0530

    constant folding

--- ico.py
+++ ico.py
@@ -3,10 +3,42 @@ from constructs.symbol_table import SymbolInfo, SymbolTable
 from icg import IntermediateCode, Quad
 from typing import List
 
-def constant_folding(code_list: List[Quad]):
+def constant_folding(code_list: List[Quad], symtab: SymbolTable):
     for line in code_list:
-        if (line.operator == '=' and line.print_info() is not None):
-            print(line.print_info())
+        # print(line.op1, line.op2)
+        if isinstance(line.dest, SymbolInfo):
+            line.dest = line.dest.name
+        elif isinstance(line.dest, Literal):
+            line.dest = line.dest.value
+
+        if isinstance(line.op1, SymbolInfo):
+            line.op1 = line.op1.name
+        # elif isinstance(line.op1, Literal):
+        #     line.op1 = line.op1.value
+        
+        if isinstance(line.op2, SymbolInfo):
+            line.op2 = line.op2.name
+        # elif isinstance(line.op2, Literal):
+        #     line.op2 = line.op2.value
+        
+        if (isinstance(line.op1, Literal) and isinstance(line.op2, Literal) and (line.operator in ['+', '-', '*', '/'] )):
+            if line.op1.type == "number" and line.op2.type == "number":
+                if (line.operator == '+'):
+                    line.op1 = line.op1.value + line.op2.value
+                elif (line.operator == '-'):
+                    line.op1 = line.op1.value - line.op2.value
+                elif (line.operator == '*'):
+                    line.op1 = line.op1.value * line.op2.value
+                elif (line.operator == '/'):
+                    line.op1 = line.op1.value / line.op2.value
+                else:
+                    continue
+                line.op2 = None
+                line.operator = '='
+                t = symtab.get_symbol(line.dest)
+                t.value = line.op1
+            print("{} = {} {} {}".format(line.dest, line.op1, line.operator, line.op2))
+
 
 def dead_code(code_list: List[Quad]):
     ignore_list = ['call_return', 'goto', 'if', 'ifFalse', 'Label: ', 'push', 'print', 'use', 'call']
@@ -56,13 +88,14 @@ def dead_code(code_list: List[Quad]):
 
     return opt_code_list
 
-def parse_ico(ic: IntermediateCode):
+def parse_ico(ic: IntermediateCode, symtab: SymbolTable):
     print("lol nub")
     # for i in ic.code_list:
     #     if i.print_info() is not None:
     #         print(i.print_info())
-    #constant_folding(ic.code_list)
-    ic.code_list = dead_code(ic.code_list)
+    constant_folding(ic.code_list, symtab)
+    print("bonjour")
+    # ic.code_list = dead_code(ic.code_list)
     return ic
 
 if __name__ == '__main__':
